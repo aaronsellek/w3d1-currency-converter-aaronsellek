37352dfe7851614615d431e74289ad5316bc6939
aaronsellek <47368480+aaronsellek@users.noreply.github.com>
2019-05-09 15:16:54 -0400

    Add files via upload

---
+++ w3d1-currency-converter-aaronsellek/convert.py
@@ -0,0 +1,10 @@
+def convert():
+    rates_list = []
+    rates_list= ("EUR", "USD"):	1.13485	
+    rates_list= ("GBP",  "EUR"): 1.15139	
+    rates_list= ("USD", "JPY"):	110.922	
+    rates_list= ("GBP", "USD"):	1.30665
+    rates_list= ("USD", "CHF"):	1.00032	
+    rates_list= ("USD", "CAD"):	1.31782	
+    rates_list= ("EUR", "JPY"):	125.880	
+    rates_list= ("AUD", "USD"):	0.71692
